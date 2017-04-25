64c92b7192b35aa266a26e23d98f748233284109
Hasan Abdullah <hasan.cse91@gmail.com>
2017-04-25 11:25:39 +0600

    Update db.php

--- db.php
+++ db.php
@@ -1,13 +1,7 @@
 <?php
 
 //For localhost
-mysql_connect("localhost", "phpmyadmin", "h") or die("Cannot connect");
+mysql_connect("localhost", "user_id", "password") or die("Cannot connect");
 mysql_select_db("cse14db") or die("Cannot select DB");
-
-/*
-//For Live Server
-mysql_connect("localhost", "dcccse1_hasan", "Qd]u_M1}1,h6") or die("Cannot connect");
-mysql_select_db("dcccse1_cse72") or die("Cannot select DB");
-*/
 
 ?>
